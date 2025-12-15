1e76bb3784b53455c90ab1c13ee264222eced4ab
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2025-12-15 17:14:40 +0530

    Create PassengerWeight.java

---
+++ PassengerWeight.java
@@ -0,0 +1,23 @@
+// 
+import java.util.*;
+
+public class PassengerWeight {
+  public static void main (String a[]){
+    Scanner sc =  new Scanner (System.in);
+  
+    
+    int cap = sc.nextInt();
+    int adult = sc.nextInt();
+    int children = sc.nextInt();
+    
+
+
+
+int total =  adult*75 +children*30;
+
+if(total <= cap)
+  System.out.println("Boat is stable");
+else
+  System.out.println("Boat  will sink");
+}
+}
