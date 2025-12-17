d6c05efba55bb3e1040ffcb6d8c64abd4fb27fa7
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2025-12-17 22:11:02 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+// 
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Ayush";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println("Reversed String: " + rev);
+    }
+}
+
+// 
