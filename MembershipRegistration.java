7eabeb5e7c7742565a5d540759e981b990379b54
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2025-12-20 20:12:58 +0530

    Create MembershipRegistration.java

---
+++ MembershipRegistration.java
@@ -0,0 +1,15 @@
+// 
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]){
+    Scanner sc =  new Scanner(System.in);
+
+    String name = sc.nextLine();
+
+    String membership = sc.nextLine();
+
+    System.out.println("Member Name: "+name);
+    System.out.println("Selected Membership: "+membership);
+  }
+}
