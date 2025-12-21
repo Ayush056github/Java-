aaf84f34bb4477590ed875f2d595fb10312ace54
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2025-12-21 23:03:17 +0530

    Create radius.java

---
+++ radius.java
@@ -0,0 +1,17 @@
+// 
+
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc =  new Scanner(System.in);
+    int d = sc.nextInt();
+
+  double r = d/2.0;
+    double area = 3.14*r*r;
+
+  System.out.printf(".2f",area);
+  }
+}
+
+// 
