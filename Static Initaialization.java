133bf7f778315404e4e91e2661e0f88979f93f90
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2025-12-28 11:43:24 +0530

    Create Static Initaialization.java

---
+++ Static Initaialization.java
@@ -0,0 +1,31 @@
+// 
+import java.io.*;
+import java.regex;
+import java.util;
+import java.math;
+
+public class Solution{
+
+static int B,H;
+static Boolean flag = true;
+static Scanner  = new Scanner(System.in);
+
+static {
+  B = sc.nextInt();
+  H = sc.nextInt();
+
+  if(B<=0 || H<=0){
+    flag = false;
+  System.out.println("java.lang.Exception: Breadth and height must be positive");
+}
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}
+
+}
