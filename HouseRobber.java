921ce3eea38a49133caa9d4ac12f8d48a1391688
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-04 21:40:27 +0530

    Create HouseRobber.java

---
+++ HouseRobber.java
@@ -0,0 +1,18 @@
+// 
+class Solution {
+    public int rob(int[] nums) {
+
+      int roobber1 = 0 ;
+      int roobber2 = 0 ;
+
+      for(int num : nums){
+        int house = roobber1 ;
+        roobber 2 = roobber 1;
+        roobber1 = house; 
+        
+        
+    }
+      return roobber1;
+}
+
+// 
