a0ef442d2b83863dbc2da46fcda7a5b77e71c480
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-12 13:35:20 +0530

    Create Sort Colors.java

---
+++ Sort Colors.java
@@ -0,0 +1,21 @@
+// 
+import java.util.Scanner;
+import java.Arrays;
+
+
+class Solution{
+   public void sortColors(int[] nums) {
+
+     Arrays.sort(nums);
+   }
+}
+
+
+public static void main(String [] args){
+  Scanner sc = new Scanner(System.in);
+  int nums = {1,1,1,2,1,2};
+
+  System.out.println(Arrays.toString(nums));
+}
+
+  
