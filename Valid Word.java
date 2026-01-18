db2f13c1a4bafcc4339005340fb85b5a6fbd68e5
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-18 12:04:13 +0530

    Create Valid Word.java

---
+++ Valid Word.java
@@ -0,0 +1,36 @@
+// 
+
+class Solution {
+    public boolean isValid(String word) {
+
+        if(word.length()  < 3 ){
+            return false ;
+        }
+
+        boolean hasVowel = false ;
+        boolean hasConsonant = false;
+
+        for(char ch : word.toCharArray()){
+
+            if(Character.isLetter(ch)) {
+                ch = Character.toLowerCase(ch);
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ){
+                    hasVowel =  true ;
+
+                }else {
+                    hasConsonant = true;
+                }
+
+            }else if(Character.isDigit(ch)){
+                     continue;
+                } else {
+                    return false ;
+                }
+
+            }
+            return  hasVowel && hasConsonant ;
+        }
+        
+
+        // 
+    }
