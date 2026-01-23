717a4b36642a7c1edc2b9b36d7b71a0f07a832ef
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-23 22:00:52 +0530

    Create StringBasic2.java

---
+++ StringBasic2.java
@@ -0,0 +1,13 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    String str = "Ayush Mathur";
+    for(int i = 0 ;  i < str.length ; i++){
+      System.out.print(str.charAt(i));
+    }
+  }
+}
+    
