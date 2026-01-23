42cfc455764866949a84748d15df522deacef755
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-23 21:56:20 +0530

    Create StringBasic1.java

---
+++ StringBasic1.java
@@ -0,0 +1,17 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String surname = sc.nextLine();
+        String s = (name +" is a Boy");
+        String t = (surname);
+        System.out.println(s);
+        System.out.println(t);
+    }
+}
+// 
