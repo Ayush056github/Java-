c66d77e97bf09f86131829e2261998272f5922ea
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-24 22:30:58 +0530

    Create StringBasic3.java

---
+++ StringBasic3.java
@@ -0,0 +1,28 @@
+// 
+public class Main{
+    public static void main(String[] args){
+        String s1 = "Ayush Mathur";
+        String s2 = "Aayush Sharma";
+        int a1 = 23;
+        int a2 = 34;
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1.indexOf("A"));
+        System.out.println(s1.lastIndexOf("u"));
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.contains("Mathur"));
+        System.out.println(s1.contains("Kumar"));
+        System.out.println(s1.endsWith("yush"));
+        System.out.println(s1.endsWith("athur"));
+        System.out.println(s1.concat(""));
+        System.out.println(compareTo(s1,s2));
+       
+        
+        
+        
+        
+    }
+}
+
+// 
