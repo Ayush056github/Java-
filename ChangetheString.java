42a0c422f6f2fe3b2f7d233d5e8d5f6d0cec0ca4
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-24 22:29:55 +0530

    Create ChangetheString.java

---
+++ ChangetheString.java
@@ -0,0 +1,14 @@
+// 
+// 
+
+class Solution {
+    String modify(String s) {
+       if (Character.isLowerCase(s.charAt(0))) {
+            return s.toLowerCase();
+            
+        } else {
+            return s.toUpperCase();
+        }
+    }
+}
+// 
