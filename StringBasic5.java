49de40af27521bfc680715282ade7533e0aefc08
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-25 13:53:15 +0530

    Create StringBasic5.java

---
+++ StringBasic5.java
@@ -0,0 +1,17 @@
+// 
+import java.util.*;
+
+public class Main{
+    public static  void main(String[] args){
+        String s = "Tere ";
+        s +="liye Maar jayenge";
+        s+= 10;
+        System.out.println("Krutika"+(10+23));
+        System.out.println(10+"Vijay"+45);
+        System.out.println(10+20+"Aayush");
+        System.out.println('A'+2+"Isha");
+        
+        System.out.println(s);
+    }
+}
+// 
