3e0b871156e4e0fb12231d2bf7860ba3296ce5e7
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-26 13:40:02 +0530

    Create Stringtoint.java

---
+++ Stringtoint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = "909098";
+        int n = Integer.parseInt(s);
+        System.out.println(n+1);
+        
+        
+    }
+}
