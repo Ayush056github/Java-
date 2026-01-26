7515c10870a5a965beaae966b67415c2c104b6ec
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-26 21:15:37 +0530

    Create StringBuilder.java

---
+++ StringBuilder.java
@@ -0,0 +1,20 @@
+// 
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+       
+        StringBuilder sb = new StringBuilder("Aayush Mathur");
+        System.out.println(sb);
+        sb.append(" Kumar");
+        System.out.println(sb);
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+        sb.setCharAt(1,'B');
+        System.out.println(sb);
+        
+    }
+
+}
+
+// 
