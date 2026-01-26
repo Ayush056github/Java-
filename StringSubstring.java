4e31ea3528323e04ccadc662e0cf8b1238c0a2ff
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-26 16:46:03 +0530

    Create StringSubstring.java

---
+++ StringSubstring.java
@@ -0,0 +1,17 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+        System.out.println(s);
+        System.out.println(s.substring(1,s.length()-1));
+        System.out.println(s.substring(3));
+        
+        
+    }
+}
+// 
