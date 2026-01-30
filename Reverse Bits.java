09edbbad0a301a3365fd2b2656dcd94d378e6678
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-30 22:16:21 +0530

    Create Reverse Bits.java

---
+++ Reverse Bits.java
@@ -0,0 +1,16 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result = result * 2 + (n % 2);
+            n = n / 2;
+        }
+        return result;
+    }
+}
+// 
