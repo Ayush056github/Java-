abf8db5df9971128ab72fe30b7b92b430266c9c0
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-31 12:09:46 +0530

    Create ReverseQueue.java

---
+++ ReverseQueue.java
@@ -0,0 +1,19 @@
+// 
+import java.util.*;
+import java.util.Queue;
+
+
+public class reverseQueue{
+  public static void main(String[] args){
+    Queue<Integer> q = new LinkedList<>();
+
+   if(q.isEmpty())return;
+
+  int x = q.poll;
+  reverseQueue(q);
+  q.add(x);
+
+  }
+}
+
+// 
