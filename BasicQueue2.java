b2939fd05546b5ef0fffcc0580c1cdafc6070214
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-31 11:49:48 +0530

    Create BasicQueue2.java

---
+++ BasicQueue2.java
@@ -0,0 +1,21 @@
+// 
+import java.util.*;
+import java.util.Queue;
+
+public class Main{
+    public static void main(String[] args){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        
+        System.out.println(q+" "+q.size()+" "+q.peek());
+        q.remove();
+        q.remove();
+        System.out.println(q+" "+q.size()+" "+q.peek());
+    }
+}
+
+// 
