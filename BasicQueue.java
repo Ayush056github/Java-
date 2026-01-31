9d502d40b38afc218361d537819e674dcf5ba564
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-01-31 11:37:42 +0530

    Create BasicQueue.java

---
+++ BasicQueue.java
@@ -0,0 +1,15 @@
+// 
+import java.util.*;
+import java.util.Queue;
+
+public class Solution{
+  public static void main(String[] args){
+    Queue<Integer> q = new LinkedList<>();
+    q.add(10);
+    q.add(20);
+    q.add(30);
+    q.add(40);
+
+  System.out.println(q);
+  }
+}
