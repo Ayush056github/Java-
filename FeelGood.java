2080f0e3e9792574eb63f7f1d1811dd76e0b2499
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-02 19:20:59 +0530

    Create FeelGood.java

---
+++ FeelGood.java
@@ -0,0 +1,16 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+
+    int[] a = new int[n];
+
+    System.out.println(a[n-k]);
+    
+  }
+}
+// 
