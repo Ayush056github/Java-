db3119ccc03bdade000749fd9d02c1bb3674932a
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-03 19:47:14 +0530

    Create SumofArray.java

---
+++ SumofArray.java
@@ -0,0 +1,20 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    int n = sc.nextInt();
+    int []arr = new int[n];
+    int sum = 0;
+
+  for(int i = 0 ; i <n;i++){
+    int arr[i] = sc.nextInt();
+  }
+  for(int i = 0 ; i < arr.length;i++){
+    int sum  = sum + arr[i];
+  }
+    System.out.println(sum);
+  }
+}
+// 
