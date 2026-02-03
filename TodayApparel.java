cc202f05e82eb6f1f5b0664a4da7a4d6f2c7a662
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-03 11:23:54 +0530

    Create TodayApparel.java

---
+++ TodayApparel.java
@@ -0,0 +1,20 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc =  new Scanner(System.in);
+    int numDays = sc.nextInt();
+    int count = 0 ;
+    
+    for(int i = 0 ;i < numDays;i++){
+      int sales = sc.nextInt();
+      if(sales > 0){
+        count++;
+      }
+    }
+    System.out.println(count);
+  }
+}
+// 
