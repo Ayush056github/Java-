5e35ed103cf55a65d8b13c968e528379c2f400a3
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-05 22:13:20 +0530

    Create MaxArray..java

---
+++ MaxArray..java
@@ -0,0 +1,21 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int[] arr = {1,2,3,4,5};
+
+
+  int max = arr[0];
+
+  for(int i =0;i<n;i++){
+    if(arr[i]>max){
+    max=arr[i];
+    }
+  }
+    System.out.println("The Array is:"+max);
+  }
+}
+    
