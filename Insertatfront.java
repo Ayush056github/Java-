8a46d9376f2081eec15d32b6c704ca84ef0ebc4e
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-11 19:41:04 +0530

    Add Insertatfront.java to concatenate two strings

---
+++ Insertatfront.java
@@ -0,0 +1,18 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+     
+ 
+            String d =sc.nextLine();
+            String result=n+d;
+        
+        
+        System.out.println(result);
+    }
+}
+// 
