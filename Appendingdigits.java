c5b4a0a05dccbb8d78c9f895d378e66ef49e0738
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-11 19:37:27 +0530

    Correct input reading and digit appending logic
    Fix input handling and calculation in Appendingdigits.java

---
+++ Appendingdigits.java
@@ -0,0 +1,16 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner();
+    int n = sc.nextInt();
+
+  int d = sc.nextInt();l
+
+    int result = n*10+d;
+
+  System.out.println(result);
+  }
+}
