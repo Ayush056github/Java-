4e9443fada2e0f7cb7fb7b323112d3ba0a43ebfc
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-13 18:54:57 +0530

    Add initial Testing.java with main method

---
+++ Testing.java
@@ -0,0 +1,13 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+
+  System.out.println("This is My mistake to jump here in the Subject "+s);
+  }
+}
+// 
