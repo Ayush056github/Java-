3d629a833f28e60ca3a3fef0ba3df507fbd09117
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-14 22:02:43 +0530

    Create SumBT.java

---
+++ SumBT.java
@@ -0,0 +1,23 @@
+
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+class Solution {
+    // Function to return sum of all nodes of a binary tree
+    static int sumBT(Node root) {
+        if(root == null)return 0;
+        int leftsum = sumBT(root.left);
+        int rightsum = sumBT(root.right);
+        return(leftsum+rightsum+root.data);
+        
+    }
+}
