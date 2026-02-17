ff59e45a7df4f54cf5be0d1946f52c9d01874d55
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-02-17 11:05:40 +0530

    Add PatternAll3.java with nested loops

---
+++ PatternAll3.java
@@ -0,0 +1,13 @@
+// 
+import java.util.*;
+public class Main{
+  public static void main(String[]  args){
+    for(int i = n;i>=0;i--){
+      for(int j = 0 ; j<=i;j++){
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+}
+// 
