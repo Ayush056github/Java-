85e27c2bce4503a2993c4697653c9eb759abd130
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-03-03 20:06:50 +0530

    Create FacePrep3.java

---
+++ FacePrep3.java
@@ -0,0 +1,38 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int [n];
+        
+        for(int i =0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+ 
+        int left = 0 ;
+        int right = n-1;
+        boolean isPalindrome = true;
+        
+        while(left<right){
+            if(arr[left]!=arr[right]){
+                isPalindrome = false;
+                break;
+            }
+                 left++;
+                right--;
+            
+               
+            
+        }
+        if(isPalindrome){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        }
+    }
+// 
