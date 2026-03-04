8b2a5e0df8ba87b6db33fe2430846e907a560453
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-03-04 10:12:07 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,22 @@
+// 
+import  java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new  Scanner(System.in);
+        int n  = sc.nextInt();
+        int [] arr=  new int [n];
+        
+        for(int i = 0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(n+"Ayush");
+      
+        
+        for(int i =0;i<n;i++){
+            System.out.println(arr[i]+"Ayush");
+        }
+    }
+}
+// 
