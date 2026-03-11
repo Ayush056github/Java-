835896e5855c001a629e02d8bebf04b659aadf5d
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-03-11 18:35:59 +0530

    Create CoinGame.java

---
+++ CoinGame.java
@@ -0,0 +1,24 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        int sum =0;
+        int min = 0;
+        
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+            for(int x : arr){
+                sum += x;
+                min = Math.min(min,sum);
+                
+            }
+        }
+        System.out.println(Math.abs(min));
+    }
+}
