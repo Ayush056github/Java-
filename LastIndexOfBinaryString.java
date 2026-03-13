6181371703d0cd0c67678e61d3ba3224b3b95b71
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-03-13 19:55:19 +0530

    Create LastIndexOfBinaryString.java

---
+++ LastIndexOfBinaryString.java
@@ -0,0 +1,16 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t  = sc.nextInt();
+        sc.nextLine();
+        
+        while(t-- >0){
+            String s = sc.nextLine();
+            System.out.println(s.lastIndexOf('1'));
+        }
+    }
+}
