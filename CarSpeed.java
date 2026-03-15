bf366adc82a26711d95d651ad44e491c1e6eb95e
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-03-15 21:45:44 +0530

    Create CarSpeed.java

---
+++ CarSpeed.java
@@ -0,0 +1,24 @@
+// 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(carspeed(n));
+    }
+    public static int carspeed(int n ){
+        int sum =0;
+        
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                sum+=i;
+                
+            }
+        }
+        return sum;
+        
+    }
+}
