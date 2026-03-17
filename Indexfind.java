f2fb11fb7653a04ec07e3d0971ef40e8e28e1225
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-03-17 20:50:37 +0530

    Create Indexfind.java

---
+++ Indexfind.java
@@ -0,0 +1,43 @@
+// 
+import java.util.*;
+
+public class Main {
+
+    public static int firstOccurrence(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;        // store answer
+                right = mid - 1;  // left side me aur search
+            } 
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        System.out.println(firstOccurrence(nums, target));
+    }
+}
