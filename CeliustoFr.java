e641bbe087ed369b97fd82751f46c85629eab1db
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-03-29 20:18:40 +0530

    Create CeliustoFr.java

---
+++ CeliustoFr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+        float ans = (5.0f/9.0f)*(n-32f);
+        System.out.println(ans);
+        
+    }
+}
