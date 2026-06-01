97bae22328781d011a76c900eadaf811968db826
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2026-06-01 17:30:40 +0530

    Implement matrix multiplication in Java

---
+++ TwoMatrixMultiplication.java
@@ -0,0 +1,51 @@
+//
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int r1 = sc.nextInt();
+        int  c1= sc.nextInt();
+        
+        int[][] A= new int[r1][c1];
+        
+        for(int i = 0;i<r1;i++){
+            for(int j=0;j<c1;j++){
+                A[r1][c1]=sc.nextInt();
+                
+            }
+        }
+        int r2 = sc.nextInt();
+        int c2 = sc.nextInt();
+        
+        int [][] B = new int[r2][c2];
+        
+        for(int i=0;i<r2;i++){
+            for(int j=0;j<c2;j++){
+                B[i][j]=sc.nextInt();
+                
+            }
+        }
+        if(c1!=r2){
+            System.out.println("It is Not Possible Brother");
+            return;
+        }
+        
+        int[][] C =  new int[r1][c2];
+        
+        for(int i =0;i<r1;i++){
+            for(int j=0;j<c2;j++){
+                for(int k =0;k<c1;k++){
+                    C[i][j] += A[i][k]*B[j][k];
+                    
+                }
+            }
+        }
+        for(int i =0;i<r1;i++){
+            for(int j=0;j<c2;j++){
+                System.out.println(C[i][j]+" ");
+                
+            }
+        }
+    }
+}
